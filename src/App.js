dbcddedded26d7cadb71ab0890e9f5f7b05a7016
haru <hrtkszk@gmail.com>
2022-01-31 19:56:10 +0900

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import React from 'react'
 
-class App extends React.Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-      <h1>ログイン</h1>
-        <div>
-          <form method="get" action="../src/receive_get.py">
-            <input type="text" name="your_name" />
-            <input type="submit" />
-          </form>
-        </div>
-      </header>
-    </div>
-    )
-  }
+function App() {
+  return (
+  <div className="App">
+    <header className="App-header">
+    <h1>ログイン</h1>
+      <div>
+        <form method="get" action="../src/receive_get.py">
+          <input type="text" name="your_name" />
+          <input type="submit" />
+        </form>
+      </div>
+    </header>
+  </div>
+  )
 }
+
 
 export default App
