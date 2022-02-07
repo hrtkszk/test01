22d776435f8f93d49805b61633df9f681e40532e
haru <hrtkszk@gmail.com>
2022-02-07 21:59:22 +0900

    update testMySQL

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -1,7 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
-$link = mysql_connect('mysql201.phy.lolipop.lan:3306', 'LAA1395778', 'test01test01');
+$server   = "mysql201.phy.lolipop.lan";              // 実際の接続値に置き換える
+$user     = "LAA1395778";                           // 実際の接続値に置き換える
+$pass     = "test01test01";                           // 実際の接続値に置き換える
+$database = "LAA1395778-test01";                      // 実際の接続値に置き換える
+
+//-------------------
+//DBに接続
+//-------------------
+$link = mysql_connect( $server, $user, $pass );
 if (!$link) {
     echo (mysql_error());
 } else {
