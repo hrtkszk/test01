95ebe4c771b4f650433f0d52da468ceb53c03d9d
haru <hrtkszk@gmail.com>
2022-02-07 15:32:55 +0900

    delete access allow in testMySQL.php

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -1,6 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: Content-Type');
 $link = mysql_connect('mysql201.phy.lolipop.lan:3306', 'LAA1395778', 'test01test01');
 if (!$link) {
     print(mysql_error());
