7b4154bde83ab2b4ad5a8f3e86e1f88a4f772ea8
haru <hrtkszk@gmail.com>
2022-02-07 15:15:43 +0900

    add sucess comment in testMySQL.php

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -4,5 +4,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 $link = mysql_connect('mysql201.phy.lolipop.lan', 'LAA1395778', 'test01test01');
 if (!$link) {
     print(mysql_error());
+} else {
+    print("success");
 }
 mysql_close($link);
