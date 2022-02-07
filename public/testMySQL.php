f31052a06eee0248f2de8d504048fb84036fab75
haru <hrtkszk@gmail.com>
2022-02-07 22:03:49 +0900

    update testSQL and build^npm

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
-$server   = "mysql201.phy.lolipop.lan";              // 実際の接続値に置き換える
+$server   = "mysql201.phy.lolipop.lan:3306";              // 実際の接続値に置き換える
 $user     = "LAA1395778";                           // 実際の接続値に置き換える
 $pass     = "test01test01";                           // 実際の接続値に置き換える
 $database = "LAA1395778-test01";                      // 実際の接続値に置き換える
