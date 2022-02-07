e9e526562b5fb19ade6fb1420a06533cc6e70430
haru <hrtkszk@gmail.com>
2022-02-07 15:36:02 +0900

    add testMySQL01.php

---
+++ public/testMySQL01.php
@@ -0,0 +1,49 @@
+<?php
+//---------------------------------------------------
+// DBに接続する
+//---------------------------------------------------
+function getConnection() {
+    $server   = "mysql201.phy.lolipop.lan";              // 実際の接続値に置き換える
+    $user     = "LAA1395778";                           // 実際の接続値に置き換える
+    $pass     = "test01test01";                           // 実際の接続値に置き換える
+    $database = "LAA1395778-test01";                      // 実際の接続値に置き換える
+ 
+    //-------------------
+    //DBに接続
+    //-------------------
+    $conn = mysql_connect( $server, $user, $pass );
+    mysql_set_charset( 'utf8', $conn );
+ 
+    //-------------------
+    // データベース選択
+    //-------------------
+    mysql_select_db( $database, $conn );
+ 
+    return $conn;
+}
+ 
+//---------------------------------------------------
+// SQLを実行する
+//---------------------------------------------------
+function execute( $conn, $sql ) {
+    $result = mysql_query( $sql, $conn );
+ 
+    //データ取得
+    mysql_data_seek( $result, 0 );
+    return mysql_fetch_array( $result, MYSQL_ASSOC );
+}
+ 
+function testMain() {
+    //sql実行
+    $conn = getConnection() ;
+    $sql  = "select * from test";
+ 
+    $rset = execute( $conn, $sql );
+    return $rset;
+}
+?>
+ 
+<!-- 検索結果をダンプする -->
+&gt;pre>
+<?php var_dump( testMain() ); ?>
+</pre%lt;
