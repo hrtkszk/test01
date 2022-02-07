ddc2c929b28eafff246a22d655c45020d0313241
haru <hrtkszk@gmail.com>
2022-02-07 15:25:19 +0900

    add port num in testMySQL.php

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
-$link = mysql_connect('mysql201.phy.lolipop.lan', 'LAA1395778', 'test01test01');
+$link = mysql_connect('mysql201.phy.lolipop.lan:3306', 'LAA1395778', 'test01test01');
 if (!$link) {
     print(mysql_error());
 } else {
