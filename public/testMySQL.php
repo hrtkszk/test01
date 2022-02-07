24b4489d58031b8534d32979dfc551c6feda38fd
haru <hrtkszk@gmail.com>
2022-02-07 21:56:47 +0900

    add header in php

--- public/testMySQL.php
+++ public/testMySQL.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
 $link = mysql_connect('mysql201.phy.lolipop.lan:3306', 'LAA1395778', 'test01test01');
 if (!$link) {
     echo (mysql_error());
