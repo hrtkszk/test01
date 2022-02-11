a4c804c2bf5d0cc3ff8346f0c30769956ecf7e6c
hrtkszk <54362661+hrtkszk@users.noreply.github.com>
2022-02-11 21:40:41 +0900

    add php to py test

--- public/test.php
+++ public/test.php
@@ -4,5 +4,11 @@
 </head>
 <body>
     こんにちは、今日の日付は、「<?php echo date("Y/m/d");?>」です。
+    <?php
+    $command="python exec_from_php.py ";
+    exec($command,$output);
+    print "$output[0]\n";
+    print "$output[1]\n";
+    ?>
 </body>
 </html>
