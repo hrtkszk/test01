d9c05e18a0ba6d323a26c3b99b92afeeea77c381
haru <hrtkszk@gmail.com>
2022-02-12 12:08:49 +0900

    update receive_get.php to integrate python

--- public/receive_get.php
+++ public/receive_get.php
@@ -3,12 +3,18 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
 $rest_json = file_get_contents("php://input"); // JSONでPOSTされたデータを取り出す
 $_POST = json_decode($rest_json, true); // JSON文字列をデコード
+$command="python exe_from_php.py";
+exec($command,$output);
+
+
 
 if(empty($_POST['text'])) {
     echo json_encode(
         [
            "error" => true,
            "message" => "Error: 入力してください。",
+           "python out1" => "python out1:".$output[0],
+           "python out2" => "python out1:".$output[1],
         ]
     ); 
 } else {
@@ -16,6 +22,8 @@ if(empty($_POST['text'])) {
         [
            "error" => false,
            "message" => 'Success: 入力されたテキスト→'.$_POST['text'],
+           "python out1" => "python out1:".$output[0],
+           "python out2" => "python out1:".$output[1],
         ]
     ); 
 }
