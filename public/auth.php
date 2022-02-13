7eb0a1dcb73fc9280f3511a53af3d6badb426ca3
haru <hrtkszk@gmail.com>
2022-02-13 11:10:13 +0900

    change dialog in auth.php

--- public/auth.php
+++ public/auth.php
@@ -2,20 +2,20 @@
 $user = 'admin';
 $password = 'pass';
 
-// if (!isset($_SERVER['PHP_AUTH_USER'])){
-//     header('WWW-Authenticate: Basic realm="Private Page"');
-//     header('HTTP/1.0 401 Unauthorized');
+if (!isset($_SERVER['PHP_AUTH_USER'])){
+    header('WWW-Authenticate: Basic realm="Private Page"');
+    header('HTTP/1.0 401 Unauthorized');
 
-//     die('このページを見るにはログインが必要です');
-// }else{
+    die('このページを見るにはログインが必要です。isset');
+}else{
     if ($_SERVER['PHP_AUTH_USER'] != $user
         || $_SERVER['PHP_AUTH_PW'] != $password){
 
         header('WWW-Authenticate: Basic realm="Private Page"');
         header('HTTP/1.0 401 Unauthorized');
-        die('このページを見るにはログインが必要です');
+        die('このページを見るにはログインが必要です。authlogic');
     }
-// }
+}
 
 ?>
 
