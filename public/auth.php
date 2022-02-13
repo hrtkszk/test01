dd29e1ef64e31e1f2995f0123f3be96a12f9935c
haru <hrtkszk@gmail.com>
2022-02-13 12:26:56 +0900

    change entirely in auth.php

--- public/auth.php
+++ public/auth.php
@@ -1,32 +1,18 @@
 <?php
-$user = 'admin';
-$password = 'pass';
 
-if (!isset($_SERVER['PHP_AUTH_USER'])){
-
-    header('WWW-Authenticate: Basic realm="Private Page"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo(var_dump($_SERVER));
-
-
-    die('このページを見るにはログインが必要です。isset');
-    exit;
-}else{
-    if ($_SERVER['PHP_AUTH_USERR'] != $user
-        || $_SERVER['PHP_AUTH_PW'] != $password){
-
-        header('WWW-Authenticate: Basic realm="Private Page"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo(var_dump($_SERVER));
-
-
-        die('このページを見るにはログインが必要です。authlogic');
-        exit;
-    }
+switch (true) {
+    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+    case $_SERVER['PHP_AUTH_USER'] !== 'admin':
+    case $_SERVER['PHP_AUTH_PW']   !== 'test':
+        header('WWW-Authenticate: Basic realm="Enter username and password."');
+        header('Content-Type: text/plain; charset=utf-8');
+        die('このページを見るにはログインが必要です');
 }
 
-?>
+header('Content-Type: text/html; charset=utf-8');
 
+?>
+<!DOCTYPE html>
 <html>
 <head><title>PHP TEST</title></head>
 <body>
