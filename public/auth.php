6205e73c8b543f476f77c7c252684a4750ff0420
haru <hrtkszk@gmail.com>
2022-02-13 11:28:28 +0900

    add exit in auth.php

--- public/auth.php
+++ public/auth.php
@@ -7,6 +7,7 @@ if (!isset($_SERVER['PHP_AUTH_USER'])){
     header('HTTP/1.0 401 Unauthorized');
 
     die('このページを見るにはログインが必要です。isset');
+    exit;
 }else{
     if ($_SERVER['PHP_AUTH_USER'] != $user
         || $_SERVER['PHP_AUTH_PW'] != $password){
@@ -14,6 +15,7 @@ if (!isset($_SERVER['PHP_AUTH_USER'])){
         header('WWW-Authenticate: Basic realm="Private Page"');
         header('HTTP/1.0 401 Unauthorized');
         die('このページを見るにはログインが必要です。authlogic');
+        exit;
     }
 }
 
