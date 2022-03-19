7f569c4a04333d483a766c13d1017a4fe2cdeb35
haru <hrtkszk@gmail.com>
2022-03-19 14:22:38 +0900

    update app.js

--- src/App.js
+++ src/App.js
@@ -31,49 +31,51 @@ function PrivateRoute({ children }) {
 
 const App = () => (
   <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/auth' element={<Authenticator />} />
-      <Route path='/suc' element={<SignUpConfirmation />} />
-      <Route path='/fpc' element={<ForgotPasswordConfirmation />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/profilesearch'
-        element={
-          <PrivateRoute>
-            <ProfileSearch />
-          </PrivateRoute>
-        }
-      />
-      <Route path='/boshu'
-        element={
-          <PrivateRoute>
-            <Boshu />
-          </PrivateRoute>
-        }
-      />
-      <Route path='/messagelist'
-        element={
-          <PrivateRoute>
-            <MessageList />
-          </PrivateRoute>
-        }
-      />
-      <Route path='/message'
-        element={
-          <PrivateRoute>
-            <Message />
-          </PrivateRoute>
-        }
-      />
-      <Route path='/myprofile'
-        element={
-          <PrivateRoute>
-            <Profile />
-          </PrivateRoute>
-        }
-      />
-      <Route path='*' element={<NoMatch />} />
-    </Routes>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/auth' element={<Authenticator />} />
+        <Route path='/suc' element={<SignUpConfirmation />} />
+        <Route path='/fpc' element={<ForgotPasswordConfirmation />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/profilesearch'
+          element={
+            <PrivateRoute>
+              <ProfileSearch />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/boshu'
+          element={
+            <PrivateRoute>
+              <Boshu />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/messagelist'
+          element={
+            <PrivateRoute>
+              <MessageList />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/message'
+          element={
+            <PrivateRoute>
+              <Message />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/myprofile'
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+      </div>
   </BrowserRouter>
 )
 
