a28cb65b0cad721e11b0dec3a92a124d7f6ab22a
haru <hrtkszk@gmail.com>
2022-04-03 14:30:55 +0900

    remove contents in requireauth in signin

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -188,9 +188,9 @@ export default function App() {
           <Route
             path="/protected"
             element={
-              // <RequireAuth>
+              <RequireAuth>
                 <ProtectedPage />
-              // </RequireAuth>
+              </RequireAuth>
             }
           />
         </Route>
@@ -283,16 +283,16 @@ function AuthStatus() {
 
 function RequireAuth({ children }) {
 // function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useAuth();
-  let location = useLocation();
+  // let auth = useAuth();
+  // let location = useLocation();
 
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+  // if (!auth.user) {
+  //   // Redirect them to the /login page, but save the current location they were
+  //   // trying to go to when they were redirected. This allows us to send them
+  //   // along to that page after they login, which is a nicer user experience
+  //   // than dropping them off on the home page.
+  //   return <Navigate to="/login" state={{ from: location }} replace />;
+  // }
 
   return children;
 }
