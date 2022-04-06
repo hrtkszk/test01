48da793967137fdd16a7590399cdf1bfe1ff29a1
haru <hrtkszk@gmail.com>
2022-04-07 07:59:00 +0900

    add console log in app.js

--- src/App.js
+++ src/App.js
@@ -225,14 +225,14 @@ function RequireAuth({ children }) {
 // function RequireAuth({ children }: { children: JSX.Element }) {
   let auth = useAuth();
   // let location = useLocation();
-
+  console.log("Auth Required")
   if (!auth.user) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
     // return <Navigate to="/login" state={{ from: location }} replace={true} />;
-    console.log("Auth Required")
+    console.log("not auth")
     return <Navigate to="/login" replace={true} />;
   }
 
