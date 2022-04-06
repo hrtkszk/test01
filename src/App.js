5479095722159d43f6d985ac014bde965a79e2e8
haru <hrtkszk@gmail.com>
2022-04-07 07:54:44 +0900

    update requireauth in app.js

--- src/App.js
+++ src/App.js
@@ -223,16 +223,18 @@ function AuthProvider({ children }) {
 
 function RequireAuth({ children }) {
 // function RequireAuth({ children }: { children: JSX.Element }) {
-  // let auth = useAuth();
-  // let location = useLocation();
+  let auth = useAuth();
+  let location = useLocation();
 
-  // if (!auth.user) {
-  //   // Redirect them to the /login page, but save the current location they were
-  //   // trying to go to when they were redirected. This allows us to send them
-  //   // along to that page after they login, which is a nicer user experience
-  //   // than dropping them off on the home page.
-  //   return <Navigate to="/login" state={{ from: location }} replace />;
-  // }
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    // return <Navigate to="/login" state={{ from: location }} replace={true} />;
+    console.log("Auth Required")
+    return <Navigate to="/login" replace={true} />;
+  }
 
   return children;
 }
