0155f16cdbf42e3465d5cae9fce1325092f1fc36
haru <hrtkszk@gmail.com>
2022-04-12 21:15:26 +0900

    try to fix error in authstatus

--- src/AuthStatus.js
+++ src/AuthStatus.js
@@ -11,6 +11,7 @@ const AuthStatus = () => {
   const { AuthInfo } = useContext(AuthContext);
 //   let auth = useAuth();
   const navigate = useNavigate();
+  console.log(AuthInfo.user)
 
   if (!AuthInfo.user) {
     return <p>You are not logged in.</p>;
