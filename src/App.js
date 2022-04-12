907af2b92efda70375f01eb3220f67dc81d78b41
haru <hrtkszk@gmail.com>
2022-04-12 18:42:30 +0900

    refactor requireauth in app.js

--- src/App.js
+++ src/App.js
@@ -26,8 +26,9 @@ import { loginContext } from './index'
 
 import { AuthProvider } from './AuthContext'
 import { ProtectedPage } from './ProtectedPage'
-import { LoginPage, useAuth } from './LoginPage'
+import { LoginPage } from './LoginPage'
 import { AuthStatus } from './AuthStatus'
+import { RequireAuth } from './RequireAuth'
 
 // function useAuth() {
 //   return true;
@@ -109,25 +110,6 @@ const App = () => {
 
 export default App;
 
-
-
-function RequireAuth({ children }) {
-// function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useAuth();
-  // let location = useLocation();
-  console.log("Auth Required")
-  if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    // return <Navigate to="/login" state={{ from: location }} replace={true} />;
-    console.log("not auth")
-    return <Navigate to="/login" replace={true} />;
-  }
-
-  return children;
-}
 
 
 
