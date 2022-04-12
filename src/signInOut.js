a614a17b000731a8cf37442db65ec9df52d15512
haru <hrtkszk@gmail.com>
2022-04-12 19:07:05 +0900

    incorporate fakeauthprovider to sign in and out

--- src/signInOut.js
+++ src/signInOut.js
@@ -1,20 +1,32 @@
-import { fakeAuthProvider } from "./auth/auth";
+// import { fakeAuthProvider } from "./auth/auth";
 
 export function signInOut(setUser) {
+    // let signin = (newUser, callback) => {
+    //     // let signin = (newUser: string, callback: VoidFunction) => {
+    //     return fakeAuthProvider.signin(() => {
+    //         setUser(newUser);
+    //         callback();
+    //     });
+    // };
     let signin = (newUser, callback) => {
         // let signin = (newUser: string, callback: VoidFunction) => {
-        return fakeAuthProvider.signin(() => {
-            setUser(newUser);
-            callback();
-        });
+        setUser(newUser);
+        isAuthenticated = true;
+        setTimeout(callback, 100); // fake async
     };
 
+    // let signout = (callback) => {
+    //     // let signout = (callback: VoidFunction) => {
+    //     return fakeAuthProvider.signout(() => {
+    //         setUser(null);
+    //         callback();
+    //     });
+    // };
     let signout = (callback) => {
         // let signout = (callback: VoidFunction) => {
-        return fakeAuthProvider.signout(() => {
-            setUser(null);
-            callback();
-        });
+        setUser(null);
+        isAuthenticated = false;
+        setTimeout(callback, 100);
     };
     return { signin, signout };
 }
