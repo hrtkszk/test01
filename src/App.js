d198e8f1762aefb3e5e3d1bab0b7e40e3361fc03
haru <hrtkszk@gmail.com>
2022-04-12 18:39:04 +0900

    refactor login page in app.js

--- src/App.js
+++ src/App.js
@@ -25,8 +25,9 @@ import Boshu from './page/Boshu'
 import NoMatch from './page/NoMatch'
 import { loginContext } from './index'
 
-import { AuthProvider, AuthContext } from './AuthContext'
+import { AuthProvider } from './AuthContext'
 import { ProtectedPage } from './ProtectedPage'
+import { LoginPage, useAuth } from './LoginPage'
 
 // function useAuth() {
 //   return true;
@@ -151,48 +152,4 @@ function RequireAuth({ children }) {
 }
 
 
-function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-function LoginPage() {
-  let navigate = useNavigate();
-  // let location = useLocation();
-  let auth = useAuth();
-  
-  let from = "/protected";
-  // let from = location.state?.from?.pathname || "/protected";
-
-  function handleSubmit(event) {
-  // function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    let formData = new FormData(event.currentTarget);
-    let username = formData.get("username");
-    // let username = formData.get("username") as string;
-
-    auth.signin(username, () => {
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
-      navigate(from, { replace: true });
-    });
-  }
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from}</p>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username: <input name="username" type="text" />
-        </label>{" "}
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-}
 
