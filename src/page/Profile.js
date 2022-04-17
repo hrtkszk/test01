0820b299d27950b5efbd43e2de6d27626de8c437
haru <hrtkszk@gmail.com>
2022-04-17 14:55:47 +0900

    update signout func in profile

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import ButtonSignOut from './ButtonSignOut'
 import Container from './Container'
 import { AuthContext } from '../index';
@@ -8,22 +9,25 @@ export function useAuth() {
 }
 const Profile = () => {
   let AuthInfo = useAuth();
+  const navigate = useNavigate();
   // const { AuthInfo } = useContext(AuthContext);
   const [AuthStat, setAuthStat] = useState(AuthInfo.isAuth);
   console.log(AuthInfo)
   console.log(AuthStat)
 
-  const signOut = () => {
+  const signOut = (callback) => {
     if (AuthStat) {
       AuthInfo.isAuth = false
       setAuthStat(AuthInfo.isAuth)
       console.log(AuthInfo)
       console.log(AuthStat)
+      setTimeout(callback, 100);
     } else {
       AuthInfo.isAuth = true
       setAuthStat(AuthInfo.isAuth)
       console.log(AuthInfo)
       console.log(AuthStat)
+      setTimeout(callback, 100);
     }
   }
 
@@ -31,9 +35,9 @@ const Profile = () => {
     <Container>
       <h1>My Profile</h1>
       <ButtonSignOut
-                title="Sign Out"
-                onClick={signOut}
-              />
+        title="Sign Out"
+        onClick={() => signOut(() => navigate("/"))}
+      />
     </Container>
   )
 }
