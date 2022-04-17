125480a494a284dc2635cc3650a8531412e76d3b
haru <hrtkszk@gmail.com>
2022-04-17 15:05:17 +0900

    try to fix error in signin 2

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import React, { useContext } from 'react';
+// import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { AuthContext } from './AuthContext';
 import { AuthContext } from './index';
