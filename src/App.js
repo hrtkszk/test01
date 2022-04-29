e68e557fe44b02a15e9d0c4bc2756e62d333847c
haru <hrtkszk@gmail.com>
2022-04-29 15:31:11 +0900

    try to fix error in nested routes

--- src/App.js
+++ src/App.js
@@ -10,25 +10,28 @@ import {
 
 import Header from './Header'
 
-import Authenticator from './auth/Authenticator'
-import SignIn from './auth/SignIn'
-import SignUpConfirmation from './auth/SignUpConfirmation'
-import ForgotPasswordConfirmation from './auth/ForgotPasswordConfirmation'
+// import Authenticator from './auth/Authenticator'
+// import SignIn from './auth/SignIn'
+// import SignUpConfirmation from './auth/SignUpConfirmation'
+// import ForgotPasswordConfirmation from './auth/ForgotPasswordConfirmation'
+// import Home from './page/Home'
+// import MessageList from './page/MessageList'
+// import Message from './page/Message'
+// import Profile from './page/Profile'
+// import ProfileSearch from './page/ProfileSearch'
+// import Boshu from './page/Boshu'
+// import NoMatch from './page/NoMatch'
+// import { ProtectedPage } from './ProtectedPage'
+// import { RequireAuth } from './RequireAuth'
 
-import Home from './page/Home'
-import MessageList from './page/MessageList'
-import Message from './page/Message'
-import Profile from './page/Profile'
-import ProfileSearch from './page/ProfileSearch'
-import Boshu from './page/Boshu'
-import NoMatch from './page/NoMatch'
+
 // import { loginContext } from './index'
 
 // import { AuthProvider } from './AuthContext'
-import { ProtectedPage } from './ProtectedPage'
+
 import { LoginPage } from './LoginPage'
 import AuthStatus from './AuthStatus'
-import { RequireAuth } from './RequireAuth'
+
 
 // function useAuth() {
 //   return true;
@@ -53,8 +56,8 @@ const App = () => {
           <AuthStatus />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/header" element={<Header />}>
-              <Route path='/auth' element={<Authenticator />} />
+            <Route path="/header" element={<Header />} />
+              {/* <Route path='/auth' element={<Authenticator />} />
               <Route path='/signin' element={<SignIn />} />
               <Route path='/suc' element={<SignUpConfirmation />} />
               <Route path='/fpc' element={<ForgotPasswordConfirmation />} />
@@ -103,7 +106,7 @@ const App = () => {
                   </RequireAuth>
                 }
               />
-            </Route>
+            </Route> */}
           </Routes>
         {/* </AuthProvider> */}
       </div>
