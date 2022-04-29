eaeec85e834071a4a271deb14871938cc8bcde37
haru <hrtkszk@gmail.com>
2022-04-29 12:45:50 +0900

    fix error in header

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 // import React, { useContext, useState } from 'react'
 import { css } from 'glamor'
 import { Link } from 'react-router-dom'
